a795eba23739f61bc84c883cd1a3408e378d65d4
iPizza <ianfigueroa507@gmail.com>
2022-06-07 12:17:20 -0300

    añadiendo datos

--- src/assets/ImagenesFrontales.js
+++ src/assets/ImagenesFrontales.js
@@ -1,7 +1,7 @@
 const ImagenesFrontales = {
   ImagenFrontal1: require(`../assets/images/imgFrontal1.jpg`),
-  ImagenFrontal2: require(`../assets/images/imgFrontal1.jpg`),
-  ImagenFrontal3: require(`../assets/images/imgFrontal1.jpg`),
+  ImagenFrontal2: require(`../assets/images/imgFrontal2.jpg`),
+  ImagenFrontal3: require(`../assets/images/imgFrontal3.jpg`),
 };
 
 export default ImagenesFrontales;
